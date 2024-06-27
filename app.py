0e71ae52955579a45cace608d8cda28eef51f59a
kirill945404 <88576576+kirill945404@users.noreply.github.com>
2024-06-27 21:27:22 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,20 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Применяем CORS ко всем маршрутам
 
-# Список машин для аренды
+# Данные об автомобилях (здесь можно добавить больше данных)
 cars = [
     {
-        'name': 'Daewoo Matiz',
+        'name': 'DAEWOO MATIZ',
         'image_url': 'https://avatars.mds.yandex.net/get-autoru-vos/6078333/834ae2ee1cd6d92602ace14d05878fdc/1200x900',
         'price_per_hour': 800
     },
-    # Добавьте здесь другие машины по аналогии
+    {
+        'name': 'Daewoo Nexia I',
+        'image_url': 'https://avatars.mds.yandex.net/get-autoru-vos/2164969/5ce63b654351614346c712548514e0f1/1200x900',
+        'price_per_hour': 1000
+    }
+    # Добавьте другие автомобили по аналогии
 ]
 
-@app.route('/')
-def index():
-    return render_template('index.html', cars=cars)
+@app.route('/api/cars', methods=['GET'])
+def get_cars():
+    return jsonify(cars)
 
 if __name__ == '__main__':
     app.run(debug=True)
